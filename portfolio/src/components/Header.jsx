fce784ca214ad6a4c4287896fca098149f19ddaf
haubaustin <haubaustin@gmail.com>
2022-04-26 06:48:58 -0700

    Begun Layout

---
+++ portfolio/src/components/Header.jsx
@@ -0,0 +1,26 @@
+
+
+const Header = () => {
+
+return (
+<div className="Header">
+    <div>
+        <h2>
+            Home
+        </h2>
+    </div>
+    <div>
+        <h2>
+            Projects
+        </h2>
+    </div>
+    <div>
+        <h2>
+            Blank
+        </h2>
+    </div>
+</div>
+    )
+}
+
+export default Header
---
+++ portfolio/src/pages/Projects.js
@@ -0,0 +1,10 @@
+const Projects = () => {
+
+return (
+<div className="Projects">
+
+</div>
+    )
+}
+
+export default Projects
