caef86cc701d575d6a40653714eaf37ccf8bfb55
haubaustin <haubaustin@gmail.com>
2022-05-02 00:40:09 -0700

    Need to fix logo, Link Ghub and Deployments

--- portfolio/src/components/Header.jsx
+++ portfolio/src/components/Header.jsx
@@ -1,13 +1,11 @@
 import { Link } from "react-router-dom"
 const resume = require("../assets/resume.pdf")
-// const logo = require("../assets/Austin Haubenschild-logos_transparent.png")
 
 
 const Header = () => {
 
 return (
 <div className="Header">
-    {/* <img src={logo} alt="" className="logo"/> */}
     <h2>
         <Link to="/" className="Nav" >Home</Link>
     </h2>
--- portfolio/src/pages/Projects.js
+++ portfolio/src/pages/Projects.js
@@ -3,10 +3,44 @@ const Projects = () => {
 return (
 <div className="Projects">
     <section className="projectSection">
-        <div className="projectCard"></div>
-        <div className="projectCard"></div>
-        <div className="projectCard"></div>
-        <div className="projectCard"></div>
+        <div className="projectCard1">
+            <h2 className="projectCardTitle">Coin Chart </h2>
+            <div className="projectDescription">
+                <p className="projectDescText">Cryptocurrency search application</p>
+                <p className="projectDescText">HTML, CSS, Javascript</p>
+                <p className="projectDescText">CoinGecko API integration, AnyChart data visualization</p>
+                <span>
+                    <button>Github Repo</button>
+                    <button>Deployed App</button>
+                </span>
+            </div>
+
+        </div>
+        <div className="projectCard2">
+            <h2 className="projectCardTitle">Cert Expert </h2>
+            <div className="projectDescription">
+                <p className="projectDescText">IT certification blog for sharing resources</p>
+                <p className="projectDescText">Mongoose, Express, React, Node</p>
+                <p className="projectDescText">JSON Web Tokens</p>
+                <span><button>Github Repo</button><button>Deployed App</button></span>
+            </div>
+        </div>
+        <div className="projectCard3">
+            <h2 className="projectCardTitle">MyLieu (Group Project)</h2>
+            <div className="projectDescription">
+                <p className="projectDescText">A Medium like blog application</p>
+                <p className="projectDescText">PostgreSQL, Express, React, Node</p>
+                <p className="projectDescText">JSON Web Tokens</p>
+                <span><button>Github Frontend</button><button>Github Backend</button><button>Deployed App</button></span>
+            </div>
+        </div>
+        <div className="projectCard4">
+            <h2 className="projectCardTitle">Final Project</h2>
+            <div className="projectDescription">
+                <p className="projectDescText">Coming May 16th</p>
+
+            </div>
+        </div>
     </section>
 </div>
     )
