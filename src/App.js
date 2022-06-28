e857c8c5e4bfc921bf462e9d83490036121adf85
haubaustin <haubaustin@gmail.com>
2022-06-28 00:30:02 -0700

    Version 2

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import './App.css';
 import Header from './components/Header'
-import Footer from './components/Footer';
 import { Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import Projects from './pages/Projects'
@@ -14,7 +13,6 @@ function App() {
         <Route path="/" element={<Home />} />
         <Route path="/projects" element={<Projects />} />
       </Routes>
-      <Footer />
     </div>
   );
 }
